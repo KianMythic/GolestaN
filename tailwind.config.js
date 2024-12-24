318a6c1a2b771dc7fc59af5783634b80397bf864
kian <kiankarami.09@gmail.com>
2024-12-24 21:43:42 +0330

    test2

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,19 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'facebook': 'url("main/images/social-icon/facebook.png")',
-        'facebook-hover': 'url("main/images/social-icon/facebookcolor.png")',
-        'linkdin': 'url("main/images/social-icon/linkidin.png")',
-        'linkdin-hover': 'url("main/images/social-icon/linkdincolor.png")',
-        'insta': 'url("main/images/social-icon/instagram.png")',
-        'insta-hover': 'url("main/images/social-icon/instagramcolor.png")',
-        'telegram': 'url("main/images/social-icon/telegram.png")',
-        'telegram-hover': 'url("main/images/social-icon/telegramcolor.png")',
-        'aparat': 'url("main/images/social-icon/aparat.png")',
-        'aparat-hover': 'url("main/images/social-icon/aparatcolor.png")',
-        'gl': 'url("main/images/social-icon/gl.png")',
-        'gl-hover': 'url("main/images/social-icon/glcolor.png")',
-        'recipe-banner': "url('main/images/page1/recipe-baner-1.png')",
+        'facebook': 'url("/main/images/social-icon/facebook.png")',
+        'facebook-hover': 'url("/main/images/social-icon/facebookcolor.png")',
+        'linkdin': 'url("/main/images/social-icon/linkidin.png")',
+        'linkdin-hover': 'url("/main/images/social-icon/linkdincolor.png")',
+        'insta': 'url("/main/images/social-icon/instagram.png")',
+        'insta-hover': 'url("/main/images/social-icon/instagramcolor.png")',
+        'telegram': 'url("/main/images/social-icon/telegram.png")',
+        'telegram-hover': 'url("/main/images/social-icon/telegramcolor.png")',
+        'aparat': 'url("/main/images/social-icon/aparat.png")',
+        'aparat-hover': 'url("/main/images/social-icon/aparatcolor.png")',
+        'gl': 'url("/main/images/social-icon/gl.png")',
+        'gl-hover': 'url("/main/images/social-icon/glcolor.png")',
+        'recipe-banner': "url('blob/main/main/images/social-icon/recipe-baner-1.png')",
       },
 
       colors: {
