9403f86331b2cbf3acd827ecd06f297a0a66d0ec
kian <kiankarami.09@gmail.com>
2025-01-05 20:48:24 +0330

    last

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,19 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'facebook': 'url("GolestaN/main/images/social-icon/facebook.png")',
-        'facebook-hover': 'url("/main/images/social-icon/facebookcolor.png")',
-        'linkdin': 'url("GolestaN/main/images/social-icon/linkidin.png")',
-        'linkdin-hover': 'url("GolestaN/main/images/social-icon/linkdincolor.png")',
-        'insta': 'url("GolestaN/main/images/social-icon/instagram.png")',
-        'insta-hover': 'url("GolestaN/main/images/social-icon/instagramcolor.png")',
-        'telegram': 'url("GolestaN/main/images/social-icon/telegram.png")',
-        'telegram-hover': 'url("GolestaN/main/images/social-icon/telegramcolor.png")',
-        'aparat': 'url("GolestaN/main/images/social-icon/aparat.png")',
-        'aparat-hover': 'url("GolestaN/main/images/social-icon/aparatcolor.png")',
-        'gl': 'url("GolestaN/main/images/social-icon/gl.png")',
-        'gl-hover': 'url("GolestaN/main/images/social-icon/glcolor.png")',
-        'recipe-banner': "url('GolestaN/main/images/page1/recipe-baner-1.png')",
+        'facebook': 'url("/Golestan/main/images/social-icon/facebook.png")',
+        'facebook-hover': 'url("/Golestan/main/images/social-icon/facebookcolor.png")',
+        'linkdin': 'url("/Golestan/main/images/social-icon/linkidin.png")',
+        'linkdin-hover': 'url("/Golestan/main/images/social-icon/linkdincolor.png")',
+        'insta': 'url("/Golestan/main/images/social-icon/instagram.png")',
+        'insta-hover': 'url("/Golestan/main/images/social-icon/instagramcolor.png")',
+        'telegram': 'url("/Golestan/main/images/social-icon/telegram.png")',
+        'telegram-hover': 'url("/Golestan/main/images/social-icon/telegramcolor.png")',
+        'aparat': 'url("/Golestan/main/images/social-icon/aparat.png")',
+        'aparat-hover': 'url("/Golestan/main/images/social-icon/aparatcolor.png")',
+        'gl': 'url("/Golestan/main/images/social-icon/gl.png")',
+        'gl-hover': 'url("/Golestan/main/images/social-icon/glcolor.png")',
+        'recipe-banner': "url('/Golestan/main/images/page1/recipe-baner-1.png')",
       },
 
 
